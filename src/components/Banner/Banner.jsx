e9a5ea35213ca1e5efa6296c46370f88f05e1026
Mysterio-O <skrabbi.019@gmail.com>
2025-06-28 15:44:29 +0600

    added pdf file to be downloaded when download resume buttonis clicked

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -146,12 +146,17 @@ const Banner = () => {
 
                     <div className='flex flex-col gap-4 items-center'>
                         <PortfolioBtn />
-                        <button className="cursor-pointer flex justify-between bg-gray-800 px-3 py-2 rounded-full text-white tracking-wider shadow-xl hover:bg-gray-900 hover:scale-105 duration-500 hover:ring-1 font-mono w-[150px]">
-                            Download Resume
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 animate-bounce">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
-                            </svg>
-                        </button>
+                        <a
+                            href="/src/assets/pdfs/resume.pdf"
+                            download="resume.pdf">
+
+                            <button className="cursor-pointer flex justify-between bg-gray-800 px-3 py-2 rounded-full text-white tracking-wider shadow-xl hover:bg-gray-900 hover:scale-105 duration-500 hover:ring-1 font-mono w-[150px]">
+                                Download Resume
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-5 h-5 animate-bounce">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 13.5 12 21m0 0-7.5-7.5M12 21V3" />
+                                </svg>
+                            </button>
+                        </a>
                     </div>
 
                 </div>
